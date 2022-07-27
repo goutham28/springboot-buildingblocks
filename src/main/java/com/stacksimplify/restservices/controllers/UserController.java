690b844deb1db6b4c9700d06b305c33b21d1dde9
rv-gpavuluri <gpavuluri@redventures.com>
2022-07-27 10:11:33 +0530

    JPA-onetoMany Commit

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -3,27 +3,35 @@ package com.stacksimplify.restservices.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.stacksimplify.restservices.entities.User;
 import com.stacksimplify.restservices.exceptions.UserExistsException;
+import com.stacksimplify.restservices.exceptions.UserNameNotFoundException;
 import com.stacksimplify.restservices.exceptions.UserNotFoundException;
 import com.stacksimplify.restservices.services.UserService;
 
 //Controller -
 @RestController
+@Validated
+@RequestMapping(value = "/users")
 public class UserController {
 
 	// Autowire the UserService
@@ -31,7 +39,7 @@ public class UserController {
 	private UserService userService;
 
 	// getAllUsers Method
-	@GetMapping("/users")
+	@GetMapping
 	public List<User> getAllUsers() {
 
 		return userService.getAllUsers();
@@ -41,8 +49,8 @@ public class UserController {
 	// Create User Method
 	// @RequestBody Annotation
 	// @PostMapping Annotation
-	@PostMapping("/users")
-	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder builder) {
+	@PostMapping
+	public ResponseEntity<Void> createUser(@Valid @RequestBody User user, UriComponentsBuilder builder) {
 		try {
 			userService.createUser(user);
 			HttpHeaders headers = new HttpHeaders();
@@ -55,8 +63,8 @@ public class UserController {
 	}
 
 	// getUserById
-	@GetMapping("/users/{id}")
-	public Optional<User> getUserById(@PathVariable("id") Long id) {
+	@GetMapping("/{id}")
+	public Optional<User> getUserById(@PathVariable("id") @Min(1) Long id) {
 
 		try {
 			return userService.getUserById(id);
@@ -67,7 +75,7 @@ public class UserController {
 	}
 
 	// updateUserById
-	@PutMapping("/users/{id}")
+	@PutMapping("/{id}")
 	public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
 
 		try {
@@ -79,15 +87,38 @@ public class UserController {
 	}
 
 	// deleteUserById
-	@DeleteMapping("/users/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteUserById(@PathVariable("id") Long id) {
 		userService.deleteUserById(id);
 	}
 
 	// getUserByUsername
-	@GetMapping("/users/byusername/{username}")
-	public User getUserByUsername(@PathVariable("username") String username) {
-		return userService.getUserByUsername(username);
+	@GetMapping("/byusername/{username}")
+	public User getUserByUsername(@PathVariable("username") String username) throws UserNameNotFoundException {
+		User user =  userService.getUserByUsername(username);
+		if(user == null)
+			throw new UserNameNotFoundException("Username: '" + username + "' not found in User repository");
+		return user;
+	
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -1,10 +1,15 @@
 package com.stacksimplify.restservices.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 //Entity 
 // and
@@ -16,9 +21,11 @@ public class User {
 	@GeneratedValue
 	private Long id;
 
+	@NotEmpty(message = "Username is Mandatory field. Please provide username")
 	@Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
 	private String username;
 
+	@Size(min = 2, message = "FirstName should have atleast 2 characters")
 	@Column(name = "FIRST_NAME", length = 50, nullable = false)
 	private String firstname;
 
@@ -33,6 +40,9 @@ public class User {
 
 	@Column(name = "SSN", length = 50, nullable = false, unique = true)
 	private String ssn;
+
+	@OneToMany(mappedBy = "user")
+	private List<Order> orders;
 
 	// No Argument Constructor
 	public User() {
@@ -104,6 +114,14 @@ public class User {
 
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 
 	// To String
